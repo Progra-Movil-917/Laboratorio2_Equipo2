301afefb181d7fa68bb10466b30b62a636582a11
David Lara <larapineda2003@gmail.com>
2024-05-13 02:38:44 -0600

    Add screen podcast.

--- lib/screens/podcast.dart
+++ lib/screens/podcast.dart
@@ -1,16 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PodcastScreen extends StatelessWidget {
-  const PodcastScreen({super.key});
+  const PodcastScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(
+      context,
+      designSize: const Size(375, 812),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Podcast'),
       ),
-      body: const Center(
-        child: Text('Pantalla para el podcast'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.w),
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                'https://images.alphacoders.com/135/thumb-1920-1350421.jpeg',
+                height: 400.h,
+                width: 400.w,
+              ),
+              const Text(
+                'Nombre del Podcast',
+                style: TextStyle(fontSize: 24),
+              ),
+              const Text(
+                'Autor del Podcast',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text('00:00'),
+                  Expanded(
+                    child: Slider(
+                      value: 0,
+                      onChanged: (double value) {},
+                      min: 0,
+                      max: 100,
+                    ),
+                  ),
+                  const Text('30:00'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconButton(
+                    // Nuevo botón de "Random"
+                    icon: const Icon(Icons.shuffle),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.fast_rewind),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.play_arrow),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.fast_forward),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.repeat),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
