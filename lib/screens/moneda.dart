fbc5cfdca803edd727a7f0bb8f7b2c98dd8a902c
Eduardo Jared Trujillo Gamero <167134639+JaredTrujillo01@users.noreply.github.com>
2024-05-12 23:11:25 -0600

    Metodos para presentar compra y venta de las monedas

--- lib/screens/moneda.dart
+++ lib/screens/moneda.dart
@@ -9,7 +9,7 @@ class MonedaScreen extends StatefulWidget {
 
 class _MonedaScreenState extends State<MonedaScreen> {
   String _selmoneda = 'dolar';
-  TextEditingController _controller = TextEditingController ();
+  TextEditingController _controller = TextEditingController();
   String _result = '';
   @override
   Widget build(BuildContext context) {
@@ -17,70 +17,94 @@ class _MonedaScreenState extends State<MonedaScreen> {
       appBar: AppBar(
         title: const Text('Cambio de Moneda'),
       ),
-      body: Padding (
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
-       child: Column (
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text ( 
-        'SeleccionarMoneda',
-        style: TextStyle ( 
-          fontSize: 18.0,
-          fontWeight: FontWeight.bold,
-        ),
-          ),
-          SizedBox(height: 16.0,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton( 
-                onPressed: () {_camMoneda('Dolar');
-                },
-                child: Text('Dolar'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'SeleccionarMoneda',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
               ),
-              ElevatedButton(
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _camMoneda('Dolar');
+                  },
+                  child: const Text('Dolar'),
+                ),
+                ElevatedButton(
                   onPressed: () {
                     _camMoneda('Euro');
                   },
-                  child: Text('Euro'),
-              ),
-            ],
-          )
-        ],
-       ),
+                  child: const Text('Euro'),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
+
   void _camMoneda(String newMoneda) {
     setState(() {
       _selmoneda = newMoneda;
-      _controller.clear(); 
-      _result = ''; 
+      _controller.clear();
+      _result = '';
     });
   }
-  Widget _buildPrecio(String tipo, String valor)
-  {
-    return Column ( 
+
+  Widget _buildPrecio(String tipo, String valor) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text ( 
+        Text(
           '$tipo:',
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 18.0,
             fontWeight: FontWeight.bold,
           ),
         ),
-        SizedBox(height: 8.0),
-        Text ( 
+        const SizedBox(height: 8.0),
+        Text(
           'L.$valor',
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 16.0,
             color: Colors.blue,
             decoration: TextDecoration.underline,
           ),
         ),
-        SizedBox(height: 16.0),
+        const SizedBox(height: 16.0),
       ],
     );
+  }
+
+  //Metodo para presentar el valor de compra de cada moneda
+  String _ValorCompra() {
+    if (_selmoneda == 'Dolar') {
+      return '24.6968';
+    } else if (_selmoneda == 'Euro') {
+      return '25.2772';
+    }
+    return '';
+  }
+
+  //Metodo para presentar el valor de venta de cada moneda
+  String _ValorVenta() {
+    if (_selmoneda == 'Dolar') {
+      return '24.8203';
+    } else if (_selmoneda == 'Euro') {
+      return '29.7719';
+    }
+    return '';
   }
 }
